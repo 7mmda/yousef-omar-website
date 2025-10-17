74111591e285f04011b3a2095fef8ca5789b9be2
7mmda <104287463+7mmda@users.noreply.github.com>
2025-10-17 09:12:48 +0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-'''import { useState, useEffect, useRef } from 'react'
+import { useState, useEffect, useRef } from 'react'
 import { BrowserRouter as Router, Routes, Route, Link, useNavigate } from 'react-router-dom'
 import { Heart, Music, Sparkles, Play, Pause } from 'lucide-react'
 import { Button } from '@/components/ui/button.jsx'
